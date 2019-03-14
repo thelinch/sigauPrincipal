a9f460d93547eb24ce091e9e1ce07d2adc608c99
zfranklin <franklin.mijahuanca@unas.edu.pe>
2019-03-13 23:04:44 -0500

    busqueda_modal_beta

--- src/app/Modules/titulos/componentAdmin/busqueda/busqueda.component.ts
+++ src/app/Modules/titulos/componentAdmin/busqueda/busqueda.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { functionsGlobal } from 'src/app/global/funciontsGlobal';
 
 @Component({
   selector: 'app-busqueda',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusquedaComponent implements OnInit {
 
-  constructor() { }
+
+  busquedaForm: FormGroup;
+  secondFormGroup: FormGroup;
+  modalBusqueda: string  = "modalBusqueda"
+  constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.busquedaForm = this._formBuilder.group({
+      dni: ["", [Validators.maxLength(8),Validators.pattern(/^[0-9_-]{8,8}$/)]],
+      nombre: [''],
+      apellidoPaterno: [''],
+      apellidoMaterno: [''],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
   }
-
+  abrirModal( idModal:string){
+    functionsGlobal.openModal(idModal)
+  }
+  cerrarModal(idModal:string){
+    functionsGlobal.closeModal(idModal)
+  }
+  filtrar(formValue:any){
+    console.log(formValue)
+  }
 }
--- src/app/Modules/titulos/titulos.module.ts
+++ src/app/Modules/titulos/titulos.module.ts
@@ -4,10 +4,12 @@ import { PrincipalComponent } from './componentAdmin/principal/principal.compone
 import { BusquedaComponent } from './componentAdmin/busqueda/busqueda.component';
 import { TitulosRoutingModule } from './titulos.routing.module';
 import { TitulosComponent } from './titulos.component';
+import { MaterialModule } from 'src/app/global/global.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PrincipalComponent, BusquedaComponent,TitulosComponent],
-  imports: [CommonModule,TitulosRoutingModule],
+  imports: [CommonModule,TitulosRoutingModule,MaterialModule, FormsModule, ReactiveFormsModule],
   exports: [],
   providers: [],
 })
