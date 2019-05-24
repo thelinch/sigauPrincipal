b26b6f15602be60b32c971673d170bbac5e134f9
zfranklin <franklin.mijahuanca@unas.edu.pe>
2019-05-23 20:04:44 -0500

    interfaz_Modulos_General

--- src/app/Modules/titulos/componentAdmin/registrobachiller/registrobachiller.component.ts
+++ src/app/Modules/titulos/componentAdmin/registrobachiller/registrobachiller.component.ts
@@ -80,7 +80,7 @@ export class RegistrobachillerComponent implements OnInit, AfterViewInit {
     ['nombre_completo', 'Especialidad', 'Facultad'];
 
 
-  
+
   handleFileInput(file: FileList) {
     this.fileToUpload = file.item(0);
 
@@ -94,31 +94,23 @@ export class RegistrobachillerComponent implements OnInit, AfterViewInit {
 
 
   data: any = [
-    {
-      name: 'John Lilki',
-      registration: 'September 14, 2013',
-      email: 'jhlilk22@yahoo.com',
-      isPremium: true,
-    },
-    {
-      name: 'Jamie Harington',
-      registration: 'January 11, 2014',
-      email: 'jamieharingonton@yahoo.com',
-      isPremium: true
-    },
-    {
-      name: 'Jill Lewis',
-      registration: 'May 11, 2014',
-      email: 'jilsewris22@yahoo.com',
-      isPremium: true
-    }
+    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
   ];
 
 
 
   //codigo del stepper (pasar una transicion de pantalla)
   panelOpenState = false;
-  isLinear = true;
+  isLinear = false;
   firstFormGroup: FormGroup;
   formularioRegistroBachiller: FormGroup;
   formularioGuardarBachiller: FormGroup;
@@ -181,13 +173,15 @@ export class RegistrobachillerComponent implements OnInit, AfterViewInit {
   /*CODIGO PARA INICIAR LOS METODOS*/
   ngOnInit() {
 
+    this.listaAlumnoExcel = new Array<alumno_registroAlumnoGraduadoTitulado>();
+
     //FILTRAR
     this.dataSource.filterPredicate = function (data, filter: string): boolean {
-      return data.persona.numero_documento.toLowerCase().includes(filter) || data.persona.nombre.toLowerCase().includes(filter) || data.persona.apellido_paterno.toLowerCase().includes(filter);
+      return data.persona.numero_documento.toLowerCase().includes(filter) || data.persona.nombre_completo.toLowerCase().includes(filter) || data.escuela_profesional.nombre.toLowerCase().includes(filter);
     };
-    this.listaAlumnoExcel = new Array<alumno_registroAlumnoGraduadoTitulado>();
     //FIN DE FILTAR
 
+    //NOTIFICACIONES
     this.notificacionService.getNotificacion().subscribe(notificacion => {
       Swal.fire({
         html: notificacion.detalle,
@@ -197,14 +191,18 @@ export class RegistrobachillerComponent implements OnInit, AfterViewInit {
         timer: 3000,
       })
     })
+    //FIN DE NOTIFICACIONES
+
     functionsGlobal.iniciarScrollSpy();
     this.dataSource.paginator = this.paginator;
     this.listaAlumnosSeleccionados = new Array();
     this.iniciarData();
     //this.listarUniversidades()
+
+
     //codigo para Stepper
     this.firstFormGroup = this.fb.group({
-      firstCtrl: ['1', Validators.required]
+      firstCtrl: ['', Validators.required]
     });
 
     //FORMULARIO PARA INGRESAR DATOS EL ALUMNO A GRADUAR
@@ -242,10 +240,6 @@ export class RegistrobachillerComponent implements OnInit, AfterViewInit {
     });
 
     this.listaAlumnoGraduadoTitulado = new Array();
-
-
-
-
   }
   /*FIN DE CODIGO PARA INICIAR LOS METODOS*/
 
